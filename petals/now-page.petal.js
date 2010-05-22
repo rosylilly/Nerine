b4466ec3692e673235cf2becd3527f776537fc4a
Sho Kusano <rosylilly@aduca.org>
2010-05-23 06:48:22 +0900

    add petals

---
+++ petals/now-page.petal.js
@@ -0,0 +1,12 @@
+/*
+ * Now Page Petal
+ * ==============================
+ * Type: Action
+ * Author: rosylilly<rosylilly@aduca.org>
+ *
+ */
+Nerine.showNowPage = function(){
+  this.showMessage(this.page + '/' + this.pages.length);
+};
+
+Nerine.keyconfig.showNowPage = [78];
